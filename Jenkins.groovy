b0fbf9859b2ee4185c5afede06273c97b72fbd2d
Manasa Chikatla <Manasa@Manasas-MacBook-Pro.local>
2023-10-28 10:41:00 -0700

    adding a new file to the branch

---
+++ Jenkins.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh 'echo "Building..."'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'echo "Testing..."'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'echo "Deploying..."'
+            }
+        }
+    }
+    post {
+        success {
+            echo 'Pipeline successful!'
+        }
+        failure {
+            echo 'Pipeline failed!'
+        }
+    }
+}
